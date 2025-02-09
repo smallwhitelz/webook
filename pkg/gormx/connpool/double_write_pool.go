cbcfe8d52e121807f9994d57be6facaa3cfe2b5d
smallwhitelz <zhanglboy1228@163.com>
2025-02-09 10:02:46 +0800

    ConnPool开启事务写法

--- pkg/gormx/connpool/double_write_pool.go
+++ pkg/gormx/connpool/double_write_pool.go
@@ -37,6 +37,41 @@ func (d *DoubleWritePool) UpdatePattern(pattern string) error {
 	}
 }
 
+func (d *DoubleWritePool) BeginTx(ctx context.Context, opts *sql.TxOptions) (gorm.ConnPool, error) {
+	// 要返回一个代表双写的事务
+	pattern := d.pattern.Load()
+	switch pattern {
+	case PatternSrcOnly:
+		src, err := d.src.(gorm.TxBeginner).BeginTx(ctx, opts)
+		return &DoubleWriteTx{src: src, l: d.l, pattern: pattern}, err
+	case PatternSrcFirst:
+		src, err := d.src.(gorm.TxBeginner).BeginTx(ctx, opts)
+		if err != nil {
+			return nil, err
+		}
+		dst, err := d.dst.(gorm.TxBeginner).BeginTx(ctx, opts)
+		if err != nil {
+			d.l.Error("双写目标表开启事务失败", logger.Error(err))
+		}
+		return &DoubleWriteTx{src: src, dst: dst, l: d.l, pattern: pattern}, nil
+	case PatternDstFirst:
+		dst, err := d.dst.(gorm.TxBeginner).BeginTx(ctx, opts)
+		if err != nil {
+			return nil, err
+		}
+		src, err := d.src.(gorm.TxBeginner).BeginTx(ctx, opts)
+		if err != nil {
+			d.l.Error("双写源表开启事务失败", logger.Error(err))
+		}
+		return &DoubleWriteTx{src: src, dst: dst, l: d.l, pattern: pattern}, nil
+	case PatternDstOnly:
+		dst, err := d.dst.(gorm.TxBeginner).BeginTx(ctx, opts)
+		return &DoubleWriteTx{dst: dst}, err
+	default:
+		return nil, errUnknownPattern
+	}
+}
+
 func (d *DoubleWritePool) PrepareContext(ctx context.Context, query string) (*sql.Stmt, error) {
 	// 这个方法没办法改写
 	// 我没办法返回一个双写的 sql.Stmt
@@ -89,6 +124,155 @@ func (d *DoubleWritePool) QueryContext(ctx context.Context, query string, args .
 
 func (d *DoubleWritePool) QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row {
 	switch d.pattern.Load() {
+	case PatternSrcOnly, PatternSrcFirst:
+		return d.src.QueryRowContext(ctx, query, args...)
+	case PatternDstOnly, PatternDstFirst:
+		return d.dst.QueryRowContext(ctx, query, args...)
+	default:
+		// 这样没有带上错误信息
+		//return &sql.Row{}
+		panic(errUnknownPattern)
+	}
+}
+
+type DoubleWriteTx struct {
+	src     *sql.Tx
+	dst     *sql.Tx
+	pattern string
+	l       logger.V1
+}
+
+func (d *DoubleWriteTx) Commit() error {
+	switch d.pattern {
+	case PatternSrcOnly:
+		return d.src.Commit()
+	case PatternSrcFirst:
+		err := d.src.Commit()
+		// 如果src提交失败了怎么办?
+		if err != nil {
+			return err
+		}
+		if d.dst != nil {
+			err1 := d.dst.Commit()
+			if err1 != nil {
+				// 只能记录日志
+				d.l.Error("目标表提交事务失败")
+			}
+		}
+		return nil
+	case PatternDstFirst:
+		err := d.dst.Commit()
+		// 如果src提交失败了怎么办?
+		if err != nil {
+			return err
+		}
+		if d.src != nil {
+			err1 := d.src.Commit()
+			if err1 != nil {
+				// 只能记录日志
+				d.l.Error("源表提交事务失败")
+			}
+		}
+		return nil
+	case PatternDstOnly:
+		return d.dst.Commit()
+	default:
+		return errUnknownPattern
+	}
+}
+
+func (d *DoubleWriteTx) Rollback() error {
+	switch d.pattern {
+	case PatternSrcOnly:
+		return d.src.Rollback()
+	case PatternSrcFirst:
+		err := d.src.Rollback()
+		// 如果src提交失败了怎么办?
+		if err != nil {
+			return err
+		}
+		if d.dst != nil {
+			err1 := d.dst.Rollback()
+			if err1 != nil {
+				// 只能记录日志
+				d.l.Error("目标表回滚失败")
+			}
+		}
+		return nil
+	case PatternDstFirst:
+		err := d.dst.Rollback()
+		// 如果src提交失败了怎么办?
+		if err != nil {
+			return err
+		}
+		if d.src != nil {
+			err1 := d.src.Rollback()
+			if err1 != nil {
+				// 只能记录日志
+				d.l.Error("源表回滚失败")
+			}
+		}
+		return nil
+	case PatternDstOnly:
+		return d.dst.Rollback()
+	default:
+		return errUnknownPattern
+	}
+}
+
+func (d *DoubleWriteTx) PrepareContext(ctx context.Context, query string) (*sql.Stmt, error) {
+	// 这个方法没办法改写
+	// 我没办法返回一个双写的 sql.Stmt
+	panic("双写模式下不支持")
+}
+
+func (d *DoubleWriteTx) ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
+	switch d.pattern {
+	case PatternSrcOnly:
+		return d.src.ExecContext(ctx, query, args...)
+	case PatternSrcFirst:
+		res, err := d.src.ExecContext(ctx, query, args...)
+		if err == nil && d.dst != nil {
+			_, err1 := d.dst.ExecContext(ctx, query, args...)
+			if err1 != nil {
+				d.l.Error("双写写入 dst 失败",
+					logger.String("sql", query),
+					logger.Error(err1))
+			}
+		}
+		return res, err
+	case PatternDstOnly:
+		return d.dst.ExecContext(ctx, query, args...)
+	case PatternDstFirst:
+		res, err := d.dst.ExecContext(ctx, query, args...)
+		// d.src != nil 开启事务开成功了
+		if err == nil && d.src != nil {
+			_, err1 := d.src.ExecContext(ctx, query, args...)
+			if err1 != nil {
+				d.l.Error("双写写入 src 失败",
+					logger.String("sql", query),
+					logger.Error(err1))
+			}
+		}
+		return res, err
+	default:
+		return nil, errUnknownPattern
+	}
+}
+
+func (d *DoubleWriteTx) QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error) {
+	switch d.pattern {
+	case PatternSrcOnly, PatternSrcFirst:
+		return d.src.QueryContext(ctx, query, args...)
+	case PatternDstOnly, PatternDstFirst:
+		return d.dst.QueryContext(ctx, query, args...)
+	default:
+		return nil, errUnknownPattern
+	}
+}
+
+func (d *DoubleWriteTx) QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row {
+	switch d.pattern {
 	case PatternSrcOnly, PatternSrcFirst:
 		return d.src.QueryRowContext(ctx, query, args...)
 	case PatternDstOnly, PatternDstFirst:
