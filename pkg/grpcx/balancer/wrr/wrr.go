3a5f164b5a197e1cf548fedec06ad48da8ffb26f
smallwhitelz <zhanglboy1228@163.com>
2025-02-16 16:56:11 +0800

    平滑轮询算法的动态调整权重

--- pkg/grpcx/balancer/wrr/wrr.go
+++ pkg/grpcx/balancer/wrr/wrr.go
@@ -72,8 +72,62 @@ func (p *Picker) Pick(info balancer.PickInfo) (balancer.PickResult, error) {
 	}, nil
 }
 
+// PickV1 动态调整权重
+func (p *Picker) PickV1(info balancer.PickInfo) (balancer.PickResult, error) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+	if len(p.conns) == 0 {
+		return balancer.PickResult{}, balancer.ErrNoSubConnAvailable
+	}
+	var totalWeight int
+	var maxCC *weightConn
+	for _, c := range p.conns {
+		c.mutex.Lock()
+		totalWeight = totalWeight + c.efficientWeight
+		c.currentWeight = c.currentWeight + c.efficientWeight
+		if maxCC == nil || maxCC.currentWeight < c.currentWeight {
+			maxCC = c
+		}
+		c.mutex.Unlock()
+	}
+	maxCC.mutex.Lock()
+	maxCC.currentWeight = maxCC.currentWeight - totalWeight
+	maxCC.mutex.Unlock()
+	return balancer.PickResult{
+		SubConn: maxCC.SubConn,
+		Done: func(info balancer.DoneInfo) {
+			maxCC.mutex.Lock()
+			defer maxCC.mutex.Unlock()
+			if info.Err != nil && maxCC.efficientWeight == 0 {
+				return
+			}
+			// MaxUint32 可以替换为你认为的最大值。
+			// 例如说你预期节点的权重是在 100 - 200 之间
+			// 那么你可以设置经过动态调整之后的权重不会超过 500。
+			if info.Err == nil && maxCC.efficientWeight >= 500 {
+				return
+			}
+			if info.Err != nil {
+				maxCC.efficientWeight--
+			} else {
+				maxCC.efficientWeight++
+			}
+			// 要在这里进一步调整weight/currentWeight
+			// failover要在这里做文章
+			// 根据调用结果的具体错误信息进行容错
+			// 1. 如果要是出发了限流
+			// 1.1 你可以考虑直接挪走这个节点，后面再挪回来
+			// 1.2 你可以考虑直接将weight/currentWeight调整到极低
+			// 2. 触发了熔断呢？
+			// 3. 降级呢？
+		},
+	}, nil
+}
+
 type weightConn struct {
 	balancer.SubConn
-	weight        int
-	currentWeight int
+	mutex           sync.Mutex
+	weight          int
+	currentWeight   int
+	efficientWeight int
 }
